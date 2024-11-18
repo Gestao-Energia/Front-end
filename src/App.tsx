4c88999db8d4a9751cd0ed2f787ea218d3a8bb0f
Arthur Winiarski Bassi <arthur.winiarski.ranger@outlook.com>
2024-11-18 00:33:42 +0000

    feat: create alert component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { BrowserRouter } from 'react-router-dom'
 import Router from './routes/router'
 import { ThemeProvider } from '@mui/material'
 import { theme } from './styles/GlobalTheme'
-import Login  from './components/Login'
+import { AlertProvider } from './hooks/useAlert'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <Login />
-      <Router />
-    </BrowserRouter>
+      <AlertProvider	>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </AlertProvider	>
     </ThemeProvider>
   )
 
