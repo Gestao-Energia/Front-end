bd103b4b4e9b74187d7f9cd56332a11ce98ddba2
pomboexe <supernerds300@gmail.com>
2024-11-17 20:34:12 -0400

    [feat] Customized Sidebar Component

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,27 +1,43 @@
+import { Link } from 'react-router-dom'
 import logo from '../../assets/logo.png'
-import styles from './SideBar.css'
+import  './SideBar.css'
+import { Avatar } from '@mui/material'
 export default function SideBar () {
     return (
-      <>
+      <div className='sideBar'>
+
         <img src={logo} alt="Logo" />
 
-        <div>
-          <img src="" alt="ProfilePicture" />
-          <h2>Nome</h2>
-          <p>email@testes.com</p>
+        <div className='user-info'>
+            <Avatar variant='square'  src='' sx={{width:72, height:81}}/>
+
+          <div className='information'>
+            <h2 id='userName'>Nome</h2>
+            <p id='userEmail'>email@testes.com</p>
+          </div>
+
         </div>
-        
-        {/* Trocar pelo Link do react router */}
-        <div>
-          <a href="">Dashboard</a>
-          <a href="">Cadastro de novos usuarios</a>
-          <a href="">Controle de acesso</a>
-          <a href="">Relatorio de consumo</a>
-          <a href="">Monitoramento de secretaria</a>
-        </div>
-        <div>
-          <a href="">Perfil</a>
-        </div>
-      </>
+
+          <ul>
+            <li>
+              <Link to={'/dashboard'}>Dashboard</Link>
+            </li>
+            <li>
+              <Link to={'/cadastro'}>Cadastro de novos usuarios</Link>
+            </li>
+            <li>
+              <Link to={'/acesso'}>Controle de acesso</Link>
+            </li>
+            <li>
+              <Link to={'/relatorio'}>Relatorio de consumo</Link>
+            </li>
+            <li>
+              <Link to={'/monitoramento'}>Monitoramento de secretaria</Link>
+            </li>
+            <li>
+              <Link to={'/perfil'}>perfil</Link>
+            </li>
+          </ul>
+      </div>
     )
 }
