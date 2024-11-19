4717dcb388305a77fa680ba0f8491e5dc980100d
Arthur Winiarski Bassi <arthur.winiarski.ranger@outlook.com>
2024-11-19 03:22:23 +0000

    feat: improve sidebar component

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,43 +1,48 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import { Avatar, List, ListItem, ListItemText, Stack, Typography } from '@mui/material'
 import logo from '../../assets/logo.png'
-import  './SideBar.css'
-import { Avatar, Typography } from '@mui/material'
-export default function SideBar () {
-    return (
-      <div className='sideBar'>
+import './SideBar.css'
+import ProFileImage from '../../assets/DefaultProfileImage.png'
 
-        <img src={logo} alt="Logo" />
+export default function SideBar() {
+  const navOptions = [
+    { route: '/dashboard', label: 'Dashboard' },
+    { route: '/register', label: 'Cadastro de novos usuarios' },
+    { route: '/accessControl', label: 'Controle de acesso' },
+    { route: '/report', label: 'Relatorio de consumo' },
+    { route: '/monitoring', label: 'Monitoramento de secretaria' },
+    { route: '/profile', label: 'Perfil' }
+  ];
 
-        <div className='user-info'>
-            <Avatar variant='square'  src='' sx={{width:72, height:81}}/>
+  return (
+    <aside className='sideBar'>
 
-          <div className='information'>
-            <Typography variant='h2'>Nome</Typography>
-            <Typography variant='body1'>email@teste.com</Typography>
-          </div>
+      <img src={logo} alt="Logo" />
 
+      <section className='user-info'>
+        <Avatar variant='rounded' src={ProFileImage} sx={{ width: 72, height: 81 }} />
+
+        <div className='information'>
+          <Typography variant='h2'>Samantha</Typography>
+          <Typography variant='body1' sx={{color: '#c5c5c5'}}>samantha@email.com</Typography>
         </div>
 
-          <ul>
-            <li>
-              <Link to={'/dashboard'}>Dashboard</Link>
-            </li>
-            <li>
-              <Link to={'/register'}>Cadastro de novos usuarios</Link>
-            </li>
-            <li>
-              <Link to={'/accessControl'}>Controle de acesso</Link>
-            </li>
-            <li>
-              <Link to={'/report'}>Relatorio de consumo</Link>
-            </li>
-            <li>
-              <Link to={'/monitoring'}>Monitoramento de secretaria</Link>
-            </li>
-            <li>
-              <Link to={'/profile'}>perfil</Link>
-            </li>
-          </ul>
-      </div>
-    )
+      </section>
+
+      <Stack direction={'column'} gap={2}>
+        {navOptions.map(option => (
+          <NavLink
+            to={option.route}
+            style={({ isActive }) => {
+              return isActive ? { color: "#FFF" } : {color: '#c5c5c5'};
+            }}
+          >
+            <Typography variant='h6'>
+              {option.label}
+            </Typography>
+          </NavLink>
+        ))}
+      </Stack>
+    </aside>
+  )
 }
