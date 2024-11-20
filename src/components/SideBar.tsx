92738c92cb67f571a52e4ce8b26829833b84c171
pomboexe <supernerds300@gmail.com>
2024-11-20 18:30:51 -0400

    [chore] pr fixes

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,8 +1,7 @@
 import { Link, NavLink } from 'react-router-dom'
 import { Avatar, List, ListItem, ListItemText, Stack, Typography } from '@mui/material'
-import logo from '../../assets/logo.png'
-import './SideBar.css'
-import ProFileImage from '../../assets/DefaultProfileImage.png'
+import logo from '../assets/logo.png'
+import ProFileImage from '../assets/DefaultProfileImage.png'
 
 export default function SideBar() {
   const navOptions = [
@@ -15,29 +14,33 @@ export default function SideBar() {
   ];
 
   return (
-    <aside className='sideBar'>
+    <aside className='sideBar' style={
+      {backgroundColor: '#2C3E50', display:'flex', flexDirection:'column', padding: '57px 0px 0px 47px', height: 'calc(100vh - 57px)', width:250, color: '#FFF'}}>
 
-      <img src={logo} alt="Logo" />
+      <img src={logo} alt="Logo"  style={{width: 88, height: 35, marginBottom: 24}}/>
 
       <section className='user-info'>
-        <Avatar variant='rounded' src={ProFileImage} sx={{ width: 72, height: 81 }} />
+        <Avatar variant='rounded' src={ProFileImage} sx={{ width: 72, height: 81, marginBottom: 7 }} />
 
         <div className='information'>
-          <Typography variant='h2'>Samantha</Typography>
+          <Typography variant='h2' sx={{marginBottom: '5px', color: '#FFF', fontSize:35}}>Samantha</Typography>
           <Typography variant='body1' sx={{color: '#c5c5c5'}}>samantha@email.com</Typography>
         </div>
 
       </section>
 
-      <Stack direction={'column'} gap={2}>
+      <Stack direction={'column'} gap={4} sx={{marginTop: 10}}>
         {navOptions.map(option => (
           <NavLink
             to={option.route}
             style={({ isActive }) => {
-              return isActive ? { color: "#FFF" } : {color: '#c5c5c5'};
+              return ({
+                color: isActive ? '#FFF' : '#c5c5c5',
+                textDecoration: "none"
+              });
             }}
           >
-            <Typography variant='h6'>
+            <Typography variant='h6' >
               {option.label}
             </Typography>
           </NavLink>
