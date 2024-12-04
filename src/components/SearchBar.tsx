2b6440359181b446eaaa6b650331c7123e58b5ae
Arthur Bassi <arthur.winiarski.ranger@outlook.com>
2024-12-04 13:43:32 -0400

    style: fix indentation

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,48 +3,43 @@ import { Box, InputAdornment, TextField } from "@mui/material";
 import { useState } from "react";
 
 interface SearchBarProps {
-    onSearch: (query: string) => void;
+  onSearch: (query: string) => void;
 }
 
-export default function SearchBar ({onSearch} : SearchBarProps){
-    const [searchTerm, setSearchTerm] = useState('')
+export default function SearchBar({ onSearch }: SearchBarProps) {
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
-        const query = event.target.value
-        setSearchTerm(query)
-        onSearch(query)
-    }
-    return (
-        <Box
-            sx={{
-                width: '100%',
-                
-            }}
-        >
-            <TextField
-                value={searchTerm}
-                onChange={handleSearchChange}
-                slotProps={{
-                    input: {
-                        startAdornment:(
-                            <InputAdornment position="start">
-                                <Search></Search>
-                            </InputAdornment>
-                        ),
-                        style : {
-                            borderRadius : '50px',
-                            height: '40px',
-                            boxSizing: 'border-box',
-                            backgroundColor : '#fff'
-                        }
-                    }
-                    
-                }}
-                variant="outlined"
-                
-            />
-        </Box>
-
-    )
-
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value;
+    setSearchTerm(query);
+    onSearch(query);
+  };
+  return (
+    <Box
+      sx={{
+        width: "100%",
+      }}
+    >
+      <TextField
+        value={searchTerm}
+        onChange={handleSearchChange}
+        slotProps={{
+          input: {
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search></Search>
+              </InputAdornment>
+            ),
+            style: {
+              borderRadius: "50px",
+              height: "40px",
+              boxSizing: "border-box",
+              backgroundColor: "#fff",
+            },
+          },
+        }}
+        variant="outlined"
+      />
+    </Box>
+  );
 }
