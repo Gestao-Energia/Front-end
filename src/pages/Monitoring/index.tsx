546a897f7256c7334e19536f4393ecbed285fc9d
andymaru <anderson.nogueira01@hotmail.com>
2024-12-18 13:20:08 -0400

    feat: add Initial structure for Gauges in Monitoring Page

--- src/pages/Monitoring/index.tsx
+++ src/pages/Monitoring/index.tsx
@@ -1,3 +1,31 @@
+import React from "react";
+import card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import { Box } from "@mui/material";
+import stack from "@mui/material/Stack";
+import { Gauge } from "@mui/x-charts/Gauge";
+
 export default function Monitoring() {
-  return <h1>Monitoring Page</h1>;
+  return (
+    <Box>
+      <Card sx={{ maxWidth: 205, maxHeight: 300 }}>
+        <Typography sx={{ textAlign: "center" }}>Secretaria</Typography>
+
+        <CardContent>
+          <Gauge
+            width={100}
+            height={100}
+            value={50}
+            valueMin={10}
+            valueMax={60}
+          />
+        </CardContent>
+      </Card>
+    </Box>
+  );
 }
